56f878ad04d8338aa43d83ec9a961cb282a83648
unknown <christianjhegggfer@gmail.com>
2023-03-07 18:34:31 +0800

    added nested nav for admin van details

--- src/pages/admin/AdminVanPhotos.jsx
+++ src/pages/admin/AdminVanPhotos.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 
 const Photos = () => {
+
+  const params = useParams()
+
+  const [van, setVan] = React.useState([])
+
+  React.useEffect( () => {
+    fetch(`/api/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => setVan(data.vans))
+  }, [params.id])
+
+
   return (
-    <div>Photos</div>
+    <div>
+      <img src={van.image} className="admin-vanroute-img"/>
+    </div>
   )
 }
 
--- src/pages/admin/AdminVanPricing.jsx
+++ src/pages/admin/AdminVanPricing.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Pricing = () => {
+
+  const params = useParams()
+
+  const [van, setVan] = React.useState([])
+
+  React.useEffect( () => {
+    fetch(`/api/vans/${params.id}`)
+      .then(res => res.json())
+      .then(data => setVan(data.vans))
+  }, [params.id])
+
   return (
-    <div>Pricing</div>
+    <div>
+      <p className='admin-vanroute-price'>
+        ${van.price}
+        <span className='admin-vanroute-day'>/day</span>
+      </p>
+    </div>
   )
 }
 
